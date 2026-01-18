eec7f18c316096dafbf88e1428da4b358c89b6b7
llevasse <elise.levasse@gmail.com>
2026-01-18 11:24:31 +0100

    [fix] issue #15

--- front/src/app/pages/login/login.ts
+++ front/src/app/pages/login/login.ts
@@ -46,17 +46,20 @@ export class Login {
     var password = (document.querySelector("#register-password-input") as HTMLInputElement).value;
     if (username && first_name && last_name && email && password){
       this.authService.register({username, firstname: first_name, lastname: last_name, email, password}).then((value)=>{
-        if (value.ok){
+        let response: Response = value;
+        if (response.ok){
           history.pushState('','', "/profile");
           this.router.navigate([`/profile`]);
         }
         else{
-          if (value['error'] != null){
-            this.loginErrorMessage.set(value['error']);
-          }
-          else{
-            this.loginErrorMessage.set(value['message']);
-          }
+          response.json().then((obj)=>{
+            if (obj['error'] != null){
+              this.registerErrorMessage.set(obj['error']);
+            }
+            else{
+              this.registerErrorMessage.set(obj['message']);
+            }
+          })
         }
       });
     }
